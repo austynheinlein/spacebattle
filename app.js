6dcba5e4132f38095471fbab7df9036ec97babfc
your name <you@example.com>
2018-06-08 16:14:03 -0700

    more space battle content

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-//INTRO
-// alert("SPACE BATTLE!")
-// //ask user if they want to play. user answers yes or no.
-// const yourAnswer = prompt("Wanna fight some aliens?", "yes/no")
-//   //if user answers yes
-//   if (yourAnswer == "yes") {
-//     //alert user, and continue
-//     alert("Niiiice. Hop in this spaceship, here comes the first alien!")
-//     console.log("user input: yes");
-//   //if user answers anything else, quit game.
-//   } else {
-//     alert("It'd be cooler if you said yes... Bye!")
-//     console.log(null);
-//   }
+
 //////////////////////////////////////////////
 //OBJECTS: GAME CHARACTERS
 
 //Create Protagonist class
-class OurShip{
+class YourShip{
   constructor(name, hull, firepower, accuracy) {
     //USS Schwarzenegger
     this.name = name
@@ -31,41 +18,41 @@ class OurShip{
     //using firepower, deduct 5 hitpoints from enemy.hull
     if (getRandomAccuracy <= this.accuracy) {
       alien.hull-=this.firepower
-      console.log("Holy Shit! You actually hit an " + alien.name + ". It now has " + alien.hull + " hitpoints.");
+      console.log("Holy Shit! You just hit an " + alien.name + ".");
     } else {
       console.log("Hey, " + this.name + "! Work on you're aim!" );
     }
   }
 }
-const hero = new OurShip("USS Schwarzenegger")
+const hero = new YourShip("USS Schwarzenegger")
 
 //Create Enemy class
-class Enemy{
+class EnemyShip{
   constructor(name, hull, firepower, accuracy) {
     //Alien Ship
     this.name = name
     // same as hitpoints
     this.hull = hull
     // number of hitpoints you subtracted from hero hitpoints, when used
-    this.firepower = 2
+    this.firepower = 4
     // your chance between 0 - 1 that you'll hit enemy
     this.accuracy = 8
   }
   attack(hero){
     if (getRandomAccuracy <= this.accuracy) {
       hero.hull-=this.firepower
-      console.log("OUCH! You've been hit by an " + alien.name + ". You now have " + hero.hull + " hitpoints.");
+      alert("OUCH! You've been hit by an " + alien.name + ".");
     } else {
-      console.log("Yoooo, that was a close one! An" + this.name + "shot at you. You still have " + hero.hull + "hitpoints.");
+      alert("Yoooo, that was a close one! An " + this.name + " shot at you.");
     }
   }
 }
 
-//generate random whole number between 3 and 6 for the alien hull
-getRandomHull=Math.floor(Math.random() * (7 - 3)) + 3;
+
+getRandomHull=Math.floor(Math.random() * (10 - 6)) + 6;
 getRandomAccuracy=Math.floor(Math.random() * (10 - 0)) + 0;
 
-const alien = new Enemy("Alien Ship", getRandomHull)
+const alien = new EnemyShip("Alien Ship", getRandomHull)
 
 // create an object called game
 class Game {
@@ -74,7 +61,7 @@ class Game {
     if (hero.hull <= 0) {
       console.log("Sorry " + hero.name + ". Your hitpoints are " + hero.hull + " Which means you've been blown into spacedust. Better luck next time!");
     } else {
-      console.log("Getting nervous? Don't worry, you still have " + hero.hull + " hitpoints.");
+      console.log("You still have " + hero.hull + " hitpoint(s).");
     }
   }
   //make a method inside the object to checkWin of hero
@@ -82,26 +69,40 @@ class Game {
     if (alien.hull <= 0) {
       console.log("Look out, Han Solo, we've got a new sharp shooter in space town!! Congrats, " + hero.name + "! You defeated an " + alien.name);
     } else {
-      console.log("Don't stop now!! This alien scum still has " + alien.hull + " hitpoints.");
+      console.log("Don't stop now!! This alien scum still has " + alien.hull + " hitpoint(s).");
     }
   }
 }
 const game = new Game ("")
 
-hero.attack(alien)
-alien.attack(hero)
-console.log(alien);
-console.log(hero);
-game.checkHeroWin(hero)
-game.checkAlienWin(alien)
+// hero.attack(alien)
+// alien.attack(hero)
+// game.checkHeroWin(hero)
+// game.checkAlienWin(alien)
 
+//PROMPTS AND ALERTS
+alert("SPACE BATTLE!")
+//ask user if they want to play. user answers yes or no.
+const firstAnswer = prompt("Wanna fight some aliens?", "yes/no")
+  //if user answers yes
+  if (firstAnswer == "yes") {
+    //alert user, and continue
+    alert("Niiiice. Hop in this spaceship, here comes the first alien!")
+    console.log("user input: yes");
+  //if user answers anything else, quit game.
+  } else {
+    alert("It'd be cooler if you said yes... Bye!")
+    console.log(null);
+  }
 
-//PROMPT
-// const yourAnswer = prompt("Launch attack on Aliens?", "yes,no")
-//   if (yourAnswer == "yes") {
-//     hero.attack(alien)
-//     // hero.attack(getRandomHull-hero.firepower)
-//     alert("Holy Shit! You actually hit the " + alien.name + ". It now has " + alien.hull + " hitpoints.")
-//   } else {
-//     console.log("bummer.");
-//     }
+  alert("Ready, Aim, FIIIIRRREEE!")
+  hero.attack(alien)
+  alert("Oops, now you're being attacked...")
+  alien.attack(hero)
+  alert("Let's see how you came out of that battle:")
+  game.checkHeroWin(hero)
+  alert("But what about the bad guys???")
+  game.checkAlienWin(alien)
+
+// keep playing until hero or alien has 0 or less hitpoints.
+// ISSUE: hitpoint are resetting each round...
